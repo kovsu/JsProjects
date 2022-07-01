c5b8d0b47a10d879ce5b4b9907ec449fe2c557a6
V <2583695112@qq.com>
2022-07-01 16:43:50 +0800

    feat: all features setup

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,8 +1,75 @@
-const container = document.querySelector(".notes__container");
+const container = document.querySelector(".notes__container-2");
+const addBtn = document.querySelector(".notes__btn-add");
 
-let item = document.createElement("div");
-item.classList.add("notes__item");
-item.innerHTML = `
+function getAllNotes() {
+  const notes = localStorage.getItem("notes");
+  return notes === null ? "[]" : notes;
+}
+
+function getIdFromLs() {
+  const id = localStorage.getItem("ids");
+  console.log(id);
+  if (id === null) {
+    localStorage.setItem("ids", 0);
+    return 0;
+  }
+  localStorage.setItem("ids", parseInt(id) + 1);
+  return parseInt(id) + 1;
+}
+
+function addNotes() {
+  const notes = JSON.parse(getAllNotes());
+  localStorage.setItem(
+    "notes",
+    JSON.stringify([
+      ...notes,
+      {
+        id: getIdFromLs(),
+        content: "",
+      },
+    ])
+  );
+  renderAllNotes();
+}
+
+function getNote(id) {
+  const notes = JSON.parse(getAllNotes());
+  const note = notes.filter((note) => note.id === id);
+  return note;
+}
+
+function saveNoteContent(id, content) {
+  const notes = JSON.parse(getAllNotes());
+  const note = notes.filter((note) => note.id === id);
+  localStorage.setItem(
+    "notes",
+    JSON.stringify([
+      ...notes.filter((note) => note.id !== id),
+      {
+        id,
+        content,
+      },
+    ])
+  );
+}
+
+function deleteNote(id) {
+  const notes = JSON.parse(getAllNotes());
+  localStorage.setItem(
+    "notes",
+    JSON.stringify(notes.filter((note) => note.id !== id))
+  );
+  renderAllNotes();
+}
+
+addBtn.addEventListener("click", () => {
+  addNotes();
+});
+
+function renderNote(note) {
+  let item = document.createElement("div");
+  item.classList.add("notes__item");
+  item.innerHTML = `
   <header class="notes__header">
     <button class="notes__btn notes__btn-search">
       <i class="fa fa-magnifying-glass"></i>
@@ -16,19 +83,52 @@ item.innerHTML = `
   </header>
   <section class="notes__content">
     <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-      dignissimos dolorum asperiores, error nam id. Unde, neque! Unde
-      ducimus veniam a, molestiae itaque, dolor, tempore animi iusto
-      perspiciatis quisquam repellat.
+     ${note.content}
     </p>
+    <textarea class="notes__textarea hidden"></textarea>
   </section>
 `;
 
-const header = item.querySelector(".notes__header");
-const btn = item.querySelector(".notes__btn-search");
+  const header = item.querySelector(".notes__header");
+  const searchBtn = item.querySelector(".notes__btn-search");
+  const editBtn = item.querySelector(".notes__btn-write");
+  const deleteBtn = item.querySelector(".notes__btn-delete");
+  const textArea = item.querySelector(".notes__textarea");
+  const content = item.querySelector(".notes__content p");
 
-btn.addEventListener("click", () => {
-  header.classList.toggle("active");
-});
+  searchBtn.addEventListener("click", () => {
+    header.classList.toggle("active");
+  });
 
-container.appendChild(item);
+  deleteBtn.addEventListener("click", () => {
+    deleteNote(note.id);
+  });
+
+  editBtn.addEventListener("click", () => {
+    if (textArea.classList.contains("hidden")) {
+      console.log(note.id);
+      const noteContent = getNote(note.id).content;
+      console.log(noteContent);
+      textArea.innerHTML = noteContent;
+    } else {
+      const nowContent = textArea.value;
+      saveNoteContent(note.id, nowContent);
+      content.innerHTML = nowContent;
+    }
+
+    textArea.classList.toggle("hidden");
+    content.classList.toggle("hidden");
+  });
+
+  container.appendChild(item);
+}
+
+function renderAllNotes() {
+  container.innerHTML = "";
+  const notes = JSON.parse(getAllNotes());
+  notes.forEach((note) => {
+    renderNote(note);
+  });
+}
+
+renderAllNotes();
